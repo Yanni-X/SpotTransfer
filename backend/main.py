5366057eb3dbda4a6c3fc6aef88101c76d744506
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-12-21 00:58:39 +0530

    Add function to retrieve video IDs from tracks and update playlist creation

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,22 @@ from spotify import get_all_tracks, get_playlist_name
 
 ytmusic = YTMusic("browser.json")
 
-playlist_link = "https://open.spotify.com/playlist/2OFALaBheaMBhD8KBalULN?si=Rj8XF6xaS02iSUXmzS6bnA"
+playlist_link = "https://open.spotify.com/playlist/2SqnCGLB0l57EgNWGvWgTP"
 tracks = get_all_tracks(playlist_link, "IN")
-name = get_playlist_name(playlist_link)
+# print(tracks[0][list(tracks[0].keys())[0]])
 
-ytm_playlist_id = ytmusic.create_playlist("test2", "test description")
+
+def get_video_ids(tracks):
+    video_ids = []
+    for track in tracks:
+        search_string = f"{track['name']} {track['artists'][0]}"
+        print(search_string)
+        video_id = ytmusic.search(search_string, filter="songs")[0]["videoId"]
+        video_ids.append(video_id)
+    return video_ids
+
+
+name = get_playlist_name(playlist_link)
+video_ids = get_video_ids(tracks)
+
+ytmusic.create_playlist(name, "", "UNLISTED", video_ids)
