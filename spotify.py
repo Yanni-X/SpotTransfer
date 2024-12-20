cb6233799814f1c668f2756bb4370e62606c7133
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-12-20 23:16:43 +0530

    return available songs from playlist

--- spotify.py
+++ spotify.py
@@ -1,28 +1,66 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import os
-
+import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 
-# Replace with your credentials
+link = "https://open.spotify.com/playlist/2OFALaBheaMBhD8KBalULN?si=Rj8XF6xaS02iSUXmzS6bnA"
+all_tracks = []
+
+
+def get_spotify_access_token(client_id, client_secret):
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret
+    }
+    
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get access token: {response.json()}")
+    
+    return response.json()["access_token"]
+
+
+def extract_playlist_id(playlist_url):
+    return playlist_url.split("/playlist/")[1].split("?")[0]
+
+def get_items(playlist_id, access_token, market):
+    url = url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?market={market}&limit=100"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    # all_tracks = []
+    
+    while url:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        for item in data["items"]:
+            track = item["track"]
+            if not track or track.get("is_local") or track.get("restrictions"):
+                continue
+            all_tracks.append({
+                "name": track["name"],
+                "artists": [artist["name"] for artist in track["artists"]],
+                "album": track["album"]["name"],
+            })
+        url = data.get("next")
+        if url == 'null':
+            break        
+
+
+
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-redirect_uri = 'http://localhost:8888/callback'  # Your redirect URI
+redirect_uri = 'http://localhost:8888/callback'
+access_token = get_spotify_access_token(client_id, client_secret)
+playlist_id = extract_playlist_id(link)
 
-# # Initialize SpotifyOAuth
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope="user-library-read"))
+get_items(playlist_id, access_token, "US")
 
-# # Get a playlist's tracks (public or user-owned)
-# playlist_id = 'YOUR_PLAYLIST_ID'
-# results = sp.playlist_tracks(playlist_id)
-# tracks = results['items']
-
-# # Print the tracks
-# for idx, track in enumerate(tracks, 1):
-#     track_name = track['track']['name']
-#     artist_name = track['track']['artists'][0]['name']
-#     print(f"{idx}. {track_name} by {artist_name}")
-
-print(client_id)
-print(client_secret)
+print(len(all_tracks))
+print(all_tracks[0])
