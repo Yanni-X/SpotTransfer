9ba2cea0da5c085c98ed0b3fef259d82a8d28de5
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-12-23 00:35:35 +0530

    Refactor frontend structure: add routing, context for playlists, and new components; remove unused CSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./pages/App.tsx";
+import CreatePlaylist from "./pages/create-playlist.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/create-playlist" element={<CreatePlaylist />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
---
+++ frontend/src/nav-bar.tsx
@@ -0,0 +1,26 @@
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router";
+
+export default function Navbar() {
+    return (
+        <nav className="flex w-full justify-between items-center mt-6 px-4 sm:px-10">
+            <Link
+                to="/"
+                className="text-lg font-medium text-black hover:text-zinc-700"
+            >
+                <span className="bg-gradient-to-r from-black via-zinc-800 to-zinc-700 text-transparent bg-clip-text font-bold text-2xl">
+                    SpotTransfer
+                </span>
+            </Link>
+            <a
+                href="https://github.com/Pushan2005/SpotTransfer"
+                className="text-black dark:text-white hover:text-zinc-700 flex items-center"
+            >
+                <FaGithub className="w-6 h-6 sm:mr-2" />
+                <span className="hidden sm:inline text-lg font-medium">
+                    GitHub
+                </span>
+            </a>
+        </nav>
+    );
+}
