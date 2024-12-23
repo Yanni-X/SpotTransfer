b55790709c7b7c4d40b1b79890f1a9dbaddc41a1
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-12-24 00:12:17 +0530

    Add temporary all origins CORS support and create a home route for server status

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
-
+from flask_cors import CORS
 from ytm import create_ytm_playlist
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/create', methods=['POST'])
 def create_playlist():
@@ -15,6 +17,12 @@ def create_playlist():
         return {"message": "Playlist created successfully!"}, 200
     except Exception as e:
         return {"message": str(e)}, 500
+    
+@app.route('/', methods=['POST'])
+def home():
+    data = request.get_json()
+    print(data.get('playlist_link'))
+    return {"message": "Server Online"}, 200
 
 if __name__ == '__main__':
     app.run(port=8080)
