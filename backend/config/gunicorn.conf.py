1c9fd876542d754817e0b881cda280eacc278df7
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2025-01-04 21:18:17 +0530

    Extend timeout to 15 minutes

--- backend/config/gunicorn.conf.py
+++ backend/config/gunicorn.conf.py
@@ -4,7 +4,7 @@ import multiprocessing
 bind = "0.0.0.0:8080"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
-timeout = 600
+timeout = 900
 
 # Worker settings
 max_requests = 1000
